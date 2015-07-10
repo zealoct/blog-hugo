86b93278a61d96d6212e6fc5d06e16c7c8f12155
zealoct <zealoct@hotmail.com>
2015-07-10 17:05:28 +0800

    add deploy script to public to Github Pages

---
+++ deploy.bat
@@ -0,0 +1,17 @@
+echo "Cleaning /public directory"
+cd public/
+git rm *
+cd ..
+
+echo "Building new pages"
+hugo_0.14_windows_amd64.exe --theme=hyde-x
+copy CNAME public\
+copy robots.txt public\
+
+echo "Git commit"
+cd public/
+git add .
+git commit -am "Site updated"
+
+echo "Pushing to Github"
+git push
